42593d89c6eae67a3710227d86323a105ee18a56
CedricDaBrique <sysnographe@gmail.com>
2021-12-06 15:55:29 +0100

    Ajouts

--- src/Tableau1.js
+++ src/Tableau1.js
@@ -7,31 +7,28 @@ class Tableau1 extends Phaser.Scene{
      * Précharge les assets
      */
     preload(){
-        //bg 2 (tout au fond et très flou)
-        this.load.image('bg2-terrain-2', 'assets/level/background-2/bg2-terrain-2.png');
-        this.load.image('bg2-tree-2', 'assets/level/background-2/bg2-tree-2.png');
+        //Arrière plan
+        this.load.image('nuage', 'assets/level/assets/nuage.png');
+
 
         //bg 1 (gris légèrement flou)
-        this.load.image('bg1-terrain-3', 'assets/level/background-1/bg-terrain-3.png');
+        this.load.image('colline', 'assets/level/assets/background.png');
 
-        //ground (premier plan noir)
-        this.load.image('gMid', 'assets/level/ground/g-mid.png');
-        this.load.image('gRight', 'assets/level/ground/g-right.png');
-        this.load.image('gTree1', 'assets/level/ground/g-tree-1.png');
+
+        // premier plan
+        this.load.image('abmillieu', 'assets/level/assets/arbrecentral.png');
+        this.load.image('sol', 'assets/level/assets/ground.png');
+        this.load.image('bosse', 'assets/level/assets/groundboss.png');
 
         //au lieu d'écrire 5 lignes quasi identiques, on charge l'herbe avec une boucle
         // ALGO : ceci est une boucle
-        for(let i=1;i<=5;i++){
-            this.load.image('g-grass-'+i, 'assets/level/ground/g-grass-'+i+'.png');
-        }
+
 
         //filtre film TODO élève : faire une boucle à la place des 3 lignes qui suivent
-        this.load.image('filterFilm1', 'assets/level/filters/film/frame-1.png');
-        this.load.image('filterFilm2', 'assets/level/filters/film/frame-2.png');
-        this.load.image('filterFilm3', 'assets/level/filters/film/frame-3.png');
+
 
         //texture au fond  TODO élève : faire une boucle pour charger les 3 images et démontrer par la même que vous savez aller au plus simple
-        this.load.image('bg-animation-a', 'assets/level/background-2/bg-animation/bg-animation-a.png');
+
 
     }
 
@@ -40,172 +37,69 @@ class Tableau1 extends Phaser.Scene{
      * TODO élèves : reproduire à l'identique assets/level/00-preview-example/sample1.jpg
      * TODO élèves : plus tard, continuez le décor vers la droite en vous servant des assets mis à votre disposition
      */
-    create(){
+    create() {
 
-        /**
-         * Fond très clair avec une trame
-         * @type {Phaser.GameObjects.Sprite}
-         */
-        let bgAnimationA=this.add.sprite(0,0, 'bg-animation-a').setOrigin(0,0);
+        let nuage=this.add.sprite(0,0, 'nuage').setOrigin(0,0);
+        this.nuage = this.add.sprite(0, 0, 'nuage').setOrigin(0, 0);
+
 
         //--------------background 2 (tout au fond et flou)--------------------
-
-        /**
-         * contient tous les éléments du background 2 (gris clair très flou)
-         * @type {Phaser.GameObjects.Container}
-         * @see https://photonstorm.github.io/phaser3-docs/Phaser.GameObjects.Container.html
-         */
-        this.bg2Container=this.add.container(0,0);
-        /**
-         * Terrain dans bg2
-         * @type {Phaser.GameObjects.Image}
-         */
-        let bg2Terrain2=this.add.image(-100,100, 'bg2-terrain-2').setOrigin(0,0);
-        this.bg2Container.add(bg2Terrain2);
-        /**
-         * Arbre dans bg2
-         * @type {Phaser.GameObjects.Image}
-         */
-        let bg2Tree2=this.add.image(400,-50, 'bg2-tree-2').setOrigin(0,0);
-        this.bg2Container.add(bg2Tree2);
-        bg2Tree2.angle=-5; //pencher l'arbre de -5 degrès
-
+        this.colline=this.add.image(-20,-50, 'colline').setOrigin(0,0);
+        this.colline.scale = 0.5
         //--------------background 1 (gris) --------------------
 
-        /**
-         * contient tous les éléments du background 1 (gris)
-         * @type {Phaser.GameObjects.Container}
-         */
-        this.bg1Container=this.add.container(0,0);
-        /**
-         * Terrain
-         * @type {Phaser.GameObjects.Image}
-         */
-        let bg1Terrain3=this.add.image(-300,200, 'bg1-terrain-3').setOrigin(0,0);
-        this.bg1Container.add(bg1Terrain3);
 
-        //-------------ground (premier plan noir)---------------------------
+        //-------------ground premier plan---------------------------
 
-        /**
-         * contient tous les éléments du premier plan (noir)
-         * @type {Phaser.GameObjects.Container}
-         */
-        this.groundContainer=this.add.container(0,0);
-        /**
-         * Arbre
-         * @type {Phaser.GameObjects.Image}
-         */
-        let tree1=this.add.image(300,350, 'gTree1').setOrigin(0,1);
-        tree1.setTintFill(0xFF0000); // pratique pour dbugger
-        this.groundContainer.add(tree1);
-        /**
-         * Terrain 1
-         * @type {Phaser.GameObjects.Image}
-         */
-        //ici on va calculer les positions
-        let gMid1=this.add.image(0,350, 'gMid').setOrigin(0,0);
-        this.groundContainer.add(gMid1);
-        /**
-         * Terrain 2
-         * @type {Phaser.GameObjects.Image}
-         */
-        let gMid2=this.add.image(gMid1.x+gMid1.width+1,350, 'gMid').setOrigin(0,0); //on rajoute 1 px pour l'exemple
-        this.groundContainer.add(gMid2);
-        /**
-         * Terrain 3
-         * @type {Phaser.GameObjects.Image}
-         */
-        let gMid3=this.add.image(gMid2.x+gMid2.width,350, 'gRight').setOrigin(0,0);
-        this.groundContainer.add(gMid3);
-        /**
-         * De l'herbe en textures qui se répète
-         * @type {Phaser.GameObjects.TileSprite}
-         */
-        let grass=this.add.tileSprite(0,370,gMid3.x+gMid3.width-40,50,'g-grass-1').setOrigin(0,1)
-        this.groundContainer.add(grass);
-        /**
-         * encore de l'herbe
-         * @type {Phaser.GameObjects.TileSprite}
-         */
-        let grass2=this.add.tileSprite(0,370,gMid3.x+gMid3.width-40,50,'g-grass-3').setOrigin(0,1)
-        this.groundContainer.add(grass2);
-        /**
-         * filtre type film au premier plan
-         * @type {Phaser.GameObjects.Sprite}
-         */
-        this.filterFilm = this.add.sprite(0, 0, 'filterFilm1').setOrigin(0,0);
-        //animation de 3 images
-        this.anims.create({
-            key: 'film',
-            frames: [
-                {key:'filterFilm1'},
-                {key:'filterFilm2'},
-                {key:'filterFilm3'},
-            ],
-            frameRate: 16,
-            repeat: -1
-        });
-        this.filterFilm.play('film');
+        this.arbremillieu=this.add.image(-20,-15, 'abmillieu').setOrigin(0,0);
+        this.arbremillieu.scale = 0.5
 
-        //TODO élève faire une animation du même genre que filter mais pour bgAnimationA
 
-        //gestion du parallaxe
-        /**
-         * Vitesse de déplacement du décor
-         * @type {number}
-         */
-        this.speed=0;
-        //initialise ce qui se passe avec le clavier
+        let bosse=this.add.image(-500,-170, 'bosse').setOrigin(0,0);
+
+
+
+
+        let sol=this.add.image(-400,0, 'sol').setOrigin(0,0);
+        let sol2=this.add.image(-800,0, 'sol').setOrigin(0,0);
+
+
+
+
+
+
+
+
         this.initKeyboard();
-        // Définit l'espace de déplacement de la caméra
-        this.cameras.main.setBounds(0, 0, 2000, 540);
-        //définit à quelles vitesse se déplacent nos différents plans
-        bgAnimationA.scrollFactorX=0;
-        this.filterFilm.scrollFactorX=0;
-        this.bg2Container.scrollFactorX=0.2;
-        this.bg1Container.scrollFactorX=0.4;
-        this.groundContainer.scrollFactorX=1;
     }
-    /**
-     * Définit ce qui se passe quand on appuie ou relache une touche du clavier
-     * ALGO : ceci est une fonction ou méthode
-     */
-    initKeyboard(){
-        let me=this;
-        this.input.keyboard.on('keydown', function(kevent)
-        {
-            switch (kevent.keyCode)
+        initKeyboard(){
+            let me=this;
+            this.input.keyboard.on('keydown', function(kevent)
             {
-                case Phaser.Input.Keyboard.KeyCodes.RIGHT:
-                    me.speed=1;
-                    break;
-                case Phaser.Input.Keyboard.KeyCodes.LEFT:
-                    me.speed=-1;
-                    break;
-            }
-        });
-        this.input.keyboard.on('keyup', function(kevent)
-        {
-            switch (kevent.keyCode)
+                switch (kevent.keyCode)
+                {
+                    case Phaser.Input.Keyboard.KeyCodes.RIGHT:
+                        me.speed=1;
+                        break;
+                    case Phaser.Input.Keyboard.KeyCodes.LEFT:
+                        me.speed=-1;
+                        break;
+                }
+            });
+            this.input.keyboard.on('keyup', function(kevent)
             {
-                case Phaser.Input.Keyboard.KeyCodes.RIGHT:
-                case Phaser.Input.Keyboard.KeyCodes.LEFT:
-                    me.speed=0;
-                    break;
-            }
-        });
+                switch (kevent.keyCode)
+                {
+                    case Phaser.Input.Keyboard.KeyCodes.RIGHT:
+                    case Phaser.Input.Keyboard.KeyCodes.LEFT:
+                        me.speed=0;
+                        break;
+                }
+            });
+        }
+
+     update(){
     }
-
-    /**
-     * Cette fonction s'exécute en boucle (à peu près 60 fois par secondes)
-     */
-    update(){
-        //déplacement de la caméra
-        this.cameras.main.scrollX+=this.speed; // on aurait pu écrire : this.cameras.main.scrollX= this.cameras.main.scrollX + this.speed;
-
-        //petit effet de vibrance sur le filtre film au tout premier plan
-        this.filterFilm.setAlpha(Phaser.Math.Between(95,100)/100)
-    }
-
-
 }
+
+
